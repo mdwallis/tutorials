64ad42a6bd3a1a37b06cd44362b07a879898652d
Michael D. Wallis <mwallis76@gmail.com>
2020-09-06 10:27:00 -0400

    Removing API key that was accidently commited for the weather app.

--- react/weather-app/src/App.js
+++ react/weather-app/src/App.js
@@ -1,22 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-} from 'react-router-dom';
 import WeatherForecast from './components/WeatherForecast/weatherForecast';
 
 function App() {
   return (
-      <Router>
-        <Switch>
-            <Route path="/">
-                <WeatherForecast />
-            </Route>
-        </Switch>
-      </Router>
+    <WeatherForecast />
   )
 }
 
